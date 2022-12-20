3d49ce7a600455589a5fbc2037844e34a4419c36
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2022-12-20 16:26:37 +0530

    Fix schema.sql

--- src/main/java/lk/ijse/dep9/dao/custom/impl/UserDAOImpl.java
+++ src/main/java/lk/ijse/dep9/dao/custom/impl/UserDAOImpl.java
@@ -5,6 +5,8 @@ import lk.ijse.dep9.dao.util.ConnectionUtil;
 import lk.ijse.dep9.entity.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,7 +20,18 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public User save(User user) {
-        return null;
+        try {
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO User (username, full_name, password) VALUES (?,?,?)");
+            stm.setString(1,user.getUsername());
+            stm.setString(2,user.getFullName());
+            stm.setString(3,user.getPassword());
+
+            stm.executeUpdate();
+            return user;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
