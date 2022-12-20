3f6ae28382046db0ba4d0f7c81730532ff0db2d4
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2022-12-20 19:24:25 +0530

    Add ExceptionHandler
    method:
    validationException

--- src/main/java/lk/ijse/dep9/app/api/UserController.java
+++ src/main/java/lk/ijse/dep9/app/api/UserController.java
@@ -2,9 +2,13 @@ package lk.ijse.dep9.app.api;
 
 import lk.ijse.dep9.app.dto.UserDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -12,8 +16,12 @@ import javax.validation.Valid;
 public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = "application/json")
-    public void createUserAccount(@Valid @RequestBody UserDTO user){
+    public void createUserAccount(@Valid @RequestBody UserDTO user/*, Errors errors*/){
         System.out.println(user);
+//        Optional<FieldError> fieldError = errors.getFieldErrors().stream().findFirst();
+//        if (fieldError.isPresent()){
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,fieldError.get().getDefaultMessage());
+//        }
 
 
     }
