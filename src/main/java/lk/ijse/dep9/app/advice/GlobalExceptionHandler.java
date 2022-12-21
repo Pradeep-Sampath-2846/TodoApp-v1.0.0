4e10aed87bcc88c38a6d76a5bc09ae32a248d5fa
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2022-12-21 11:50:48 +0530

    Handle Validation Exception smoothly.

--- src/main/java/lk/ijse/dep9/app/advice/GlobalExceptionHandler.java
+++ src/main/java/lk/ijse/dep9/app/advice/GlobalExceptionHandler.java
@@ -1,26 +1,27 @@
 package lk.ijse.dep9.app.advice;
 
-import lk.ijse.dep9.app.dto.ErrorResponseMsgDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Throwable.class)
-    public ErrorResponseMsgDTO uncaughtExceptions(Throwable t){
-        t.printStackTrace();
-        return new ErrorResponseMsgDTO(t.getMessage(),405);
-    }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponseMsgDTO validationException(MethodArgumentNotValidException e){
-        String message = e.getFieldErrors().stream().map(err -> err.getField() + ": " + err.getDefaultMessage() + ", ")
-                .reduce((prev, curr) -> prev + curr).get();
-
-        return new ErrorResponseMsgDTO(message,400);
+    public Map<String,Object> validationException(MethodArgumentNotValidException e){
+        HashMap<String, Object> errAttributes = new LinkedHashMap<>();
+        errAttributes.put("status",HttpStatus.BAD_REQUEST.value());
+        errAttributes.put("error",HttpStatus.BAD_REQUEST.getReasonPhrase());
+        errAttributes.put("timeStamp",new Date().toString());
+        List<String> validationList = e.getFieldErrors().stream().map(err -> err.getField() + ": " + err.getDefaultMessage()).collect(Collectors.toList());
+        errAttributes.put("errors",validationList);
+        return errAttributes;
     }
 }
