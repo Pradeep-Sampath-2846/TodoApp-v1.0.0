ba18a96222b2fb410111077eba2617adaa357b5c
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2022-12-21 19:15:00 +0530

    Add apache common codec dependency

--- src/main/java/lk/ijse/dep9/app/api/filter/SecurityFilter.java
+++ src/main/java/lk/ijse/dep9/app/api/filter/SecurityFilter.java
@@ -1,16 +1,48 @@
 package lk.ijse.dep9.app.api.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class SecurityFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        System.out.println("Incoming request");
+       if (req.getServletPath().matches("/api/v1/users/?") && req.getMethod().equals("POST")){
+           chain.doFilter(req,res);
+       }else if (req.getServletPath().matches("/api/v1/auth/.+")){
+           chain.doFilter(req,res);
+       }else {
+
+           String authorization = req.getHeader("Authorization");
+           if (authorization!=null && authorization.startsWith("Basic")){
+               String token = authorization.substring("Basic".length() + 1);
+               String credentials = new String(Base64.getDecoder().decode(token));
+               String username = credentials.split(":")[0];
+               String password = credentials.split(":")[1];
+
+               System.out.println(username+": "+password);
+           }
+
+           HashMap<String, Object> errAttributes = new LinkedHashMap<>();
+           errAttributes.put("status", HttpStatus.UNAUTHORIZED.value());
+           errAttributes.put("error",HttpStatus.UNAUTHORIZED.getReasonPhrase());
+           errAttributes.put("message","Invalid login credentials!");
+           errAttributes.put("timeStamp",new Date().toString());
+           res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+           ObjectMapper objectMapper = new ObjectMapper();
+           res.setContentType("application/json");
+           objectMapper.writeValue(res.getWriter(),errAttributes);
+       }
     }
 }
