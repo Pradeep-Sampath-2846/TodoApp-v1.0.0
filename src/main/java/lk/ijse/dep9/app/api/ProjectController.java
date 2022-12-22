667d47653f9fb49001ad3986b063636b85decf7f
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2022-12-22 13:50:40 +0530

    finalize the project Controller structure

--- src/main/java/lk/ijse/dep9/app/api/ProjectController.java
+++ src/main/java/lk/ijse/dep9/app/api/ProjectController.java
@@ -1,7 +1,19 @@
 package lk.ijse.dep9.app.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
+@RequestMapping("/api/v1/projects")
 @RestController
 public class ProjectController {
+    private void createNewProject(){}
+
+    private void getAllProjects(){}
+
+    private void getProject(){}
+
+    private void renameProject(){}
+
+    private void deleteProject(){}
 }
